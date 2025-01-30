8fea265331733e3ddba39ce14825a4739e494b40
LeandroBryto <leandrobarreto.barreto321@gmail.com>
2025-01-30 19:53:43 -0300

    implementar serviço para adicionar transações com validação

--- src/main/java/com/leandrodev/transacao_api/business/services/TransacaoService.java
+++ src/main/java/com/leandrodev/transacao_api/business/services/TransacaoService.java
@@ -1,13 +1,35 @@
 package com.leandrodev.transacao_api.business.services;
 
+import com.leandrodev.transacao_api.controller.dto.TransacaoRequestDTO;
+import com.leandrodev.transacao_api.infrastructure.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TransacaoService {
 
-    private final List<>
+    private final List<TransacaoRequestDTO>listaTransacoes = new ArrayList<>();
+
+    public void adicionarTransacoes(TransacaoRequestDTO dto){
+
+        log.info("Inciado o processamento de gravar transações ");
+
+        if (dto.dataHora().isAfter(OffsetDateTime.now())){
+            log.error("Data e hora maiores que a data atual");
+            throw new UnprocessableEntity("Data e Hora maiores que a data  e hora atuais ");
+        }
+        if (dto.valor() < 0){
+            log.error("Valor não pode ser menor que 0");
+            throw new UnprocessableEntity("Valor não pode ser menor que 0");
+        }
+
+        listaTransacoes.add(dto);
+    }
 }
